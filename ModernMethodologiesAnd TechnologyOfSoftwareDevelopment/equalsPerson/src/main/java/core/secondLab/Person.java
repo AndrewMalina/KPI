e9ea8ba28e9a613bde855cd1bc55fff9d395dfc9
Andrew Malina <andre18malina@gmail.com>
2017-10-04 06:31:25 +0300

    Src: second java lab
    create project and include Maven framework and include Gson. Completely completed the task of serialization and deserialization + object comparison. It remains to perform unit tests

---
+++ ModernMethodologiesAnd TechnologyOfSoftwareDevelopment/equalsPerson/src/main/java/core/secondLab/Person.java
@@ -0,0 +1,52 @@
+package core.secondLab;
+
+public class Person {
+    private int age;
+    private String name;
+    private String surname;
+
+    public Person(int age, String name, String surname) {
+        this.age = age;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person that = (Person) o;
+
+        return getAge() == that.getAge() && getName().equals(that.getName()) && getSurname().equals(that.getSurname());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getAge();
+        result = 31 * result + getName().hashCode();
+        result = 31 * result + getSurname().hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+}
